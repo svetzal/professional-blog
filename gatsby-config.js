e3e74ba12573c8d33bc7fd0403f8caa6f9bad723
Stacey Vetzal <stacey@vetzal.com>
2019-12-07 06:50:32 -0500

    initial organization

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,17 +7,17 @@ module.exports = {
   ],
   // Customize your site metadata:
   siteMetadata: {
-    title: `My Blog Title`,
-    author: `My Name`,
-    description: `My site description...`,
+    title: `Stacey On Software`,
+    author: `Stacey Vetzal`,
+    description: `I'm not complicated, I just live in a complicated world.`,
     social: [
       {
         name: `twitter`,
-        url: `https://twitter.com/gatsbyjs`,
+        url: `https://twitter.com/svetzal`,
       },
       {
         name: `github`,
-        url: `https://github.com/gatsbyjs`,
+        url: `https://github.com/svetzal`,
       },
     ],
   },
